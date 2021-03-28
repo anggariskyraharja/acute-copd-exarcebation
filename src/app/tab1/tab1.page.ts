a5b079ab7c78faec61d6cb1e25448b4219584a5d
Putu Angga Risky Raharja <anggariskyraharja@gmail.com>
2021-03-28 23:11:14 +0700

    last

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,6 @@
+import { ɵDomAdapter } from '@angular/common';
 import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab1',
@@ -6,7 +8,86 @@ import { Component } from '@angular/core';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
+  score: number = 0;
+  classification1: string = 'Please answer all of the questions!';
+  classification2: string;
+  classification3: string;
+  classification4: string;
+  q1: string;
+  q2: string;
+  q3: string;
+  q4: string;
 
-  constructor() {}
+  scoring(){
+    if (this.q1 == 'q11'){
+      this.score = this.score + 1;
+    }
+    if (this.q2 == 'q11'){
+      this.score = this.score + 1;
+    }
+    if (this.q3 == 'q11'){
+      this.score = this.score + 1;
+    }
+    if (this.score == 0){
+      if (this.q4 == 'q11'){
+        this.classification1 = 'Class I, BAP-65 score = 0';
+        this.classification2 = 'The in-hospital mortality risk is 0.3%.';
+        this.classification3 = 'The need for mechanical ventilation within 48 hours is 0.3%.';
+        this.classification4 = 'Recommendation: routine management of COPD exacerbation';
+      } else if (this.q4 == 'q12'){
+        this.classification1 = 'Class II, BAP-65 score = 0';
+        this.classification2 = 'The in-hospital mortality risk is 1.0%.';
+        this.classification3 = 'The need for mechanical ventilation within 48 hours is 0.2%.';
+        this.classification4 = 'Recommendation: routine management of COPD exacerbation';
+      }
+    } else if (this.score == 1){
+      this.classification1 = 'Class III, BAP-65 score = 1';
+      this.classification2 = 'The in-hospital mortality risk is 2.2%.';
+      this.classification3 = 'The need for mechanical ventilation within 48 hours is 1.2%.';
+      this.classification4 = 'Recommendation: consider early non-invasive ventilation and/or ICU care';
+    } else if (this.score == 2){
+      this.classification1 = 'Class IV, BAP-65 score = 2';
+      this.classification2 = 'The in-hospital mortality risk is 6.4%.';
+      this.classification3 = 'The need for mechanical ventilation within 48 hours is 5.5%.';
+      this.classification4 = 'Recommendation: consider early non-invasive ventilation and/or ICU care';
+    } else if (this.score == 3){
+      this.classification1 = 'Class V, BAP-65 score = 3';
+      this.classification2 = 'The in-hospital mortality risk is 14.1%.';
+      this.classification3 = 'The need for mechanical ventilation within 48 hours is 12.4%.';
+      this.classification4 = 'Recommendation: consider early non-invasive ventilation and/or ICU care';
+    }
+  }
+  checkq1(event) {
+    this.q1 = event.target.value;
+    this.score = 0;
+    if (this.q1 == null || this.q2 == null || this.q3 == null || this.q4 == null){this.classification1='Please answer all of the questions!'}else{this.scoring();};
+  }
+  checkq2(event) {
+    this.q2 = event.target.value;
+    this.score = 0;
+    if (this.q1 == null || this.q2 == null || this.q3 == null || this.q4 == null){this.classification1='Please answer all of the questions!'}else{this.scoring();};
+  }
+  checkq3(event) {
+    this.q3 = event.target.value;
+    this.score = 0;
+    if (this.q1 == null || this.q2 == null || this.q3 == null || this.q4 == null){this.classification1='Please answer all of the questions!'}else{this.scoring();};
+  }
+  checkq4(event) {
+    this.q4 = event.target.value;
+    this.score = 0;
+    if (this.q1 == null || this.q2 == null || this.q3 == null || this.q4 == null){this.classification1='Please answer all of the questions!'}else{this.scoring();};
+  }
+
+  async presentAlert() {
+    const alert = await this.alertController.create({
+      header: 'Missing Value',
+      message: 'Please input all the required values!',
+      buttons: ['Okay']
+    });
+    await alert.present();
+  }
+
+  constructor(public alertController: AlertController) {
+  }
 
 }
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,4 +1,6 @@
+import { ɵDomAdapter } from '@angular/common';
 import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab2',
@@ -6,7 +8,108 @@ import { Component } from '@angular/core';
   styleUrls: ['tab2.page.scss']
 })
 export class Tab2Page {
+  score: number = 0;
+  classification1: string = 'Please answer all of the questions!';
+  classification2: string;
+  classification3: string;
+  classification4: string;
+  q1: string = '1';
+  q2: string;
+  q3: string;
+  q4: string;
+  q5: string;
 
-  constructor() {}
+  scoring(){
+    if (this.q1 == '2'){
+      this.score = this.score + 1;
+    } else if (this.q1 == '3'){
+      this.score = this.score + 2;
+    }
+    if (this.q2 == 'q11'){
+      this.score = this.score + 1;
+    }
+    if (this.q3 == 'q11'){
+      this.score = this.score + 1;
+    }
+    if (this.q4 == 'q11'){
+      this.score = this.score + 1;
+    }
+    if (this.q5 == 'q11'){
+      this.score = this.score + 1;
+    }
+    if (this.score == 0) {
+      this.classification1 = "DECAF score: 0 point."
+      this.classification2 = "Risk: low risk."
+      this.classification3 = "Recommendation: routine management."
+      this.classification4 = "In-hospital mortality rate: 0%"
+    } else if (this.score == 1) {
+      this.classification1 = "DECAF score: 1 point."
+      this.classification2 = "Risk: low risk."
+      this.classification3 = "Recommendation: routine management."
+      this.classification4 = "In-hospital mortality rate: 1.5%"
+    } else if (this.score == 2) {
+      this.classification1 = "DECAF score: 2 point."
+      this.classification2 = "Risk: intermediate risk."
+      this.classification3 = "Recommendation: use clinician judgment re: disposition."
+      this.classification4 = "In-hospital mortality rate: 5.4%"
+    } else if (this.score == 3) {
+      this.classification1 = "DECAF score: 3 point."
+      this.classification2 = "Risk: high risk."
+      this.classification3 = "Recommendation: consider escalation of care vs. palliative care."
+      this.classification4 = "In-hospital mortality rate: 15.3%"
+    } else if (this.score == 4) {
+      this.classification1 = "DECAF score: 4 point."
+      this.classification2 = "Risk: high risk."
+      this.classification3 = "Recommendation: consider escalation of care vs. palliative care."
+      this.classification4 = "In-hospital mortality rate: 31%"
+    }  else if (this.score == 5) {
+      this.classification1 = "DECAF score: 5 point."
+      this.classification2 = "Risk: high risk."
+      this.classification3 = "Recommendation: consider escalation of care vs. palliative care."
+      this.classification4 = "In-hospital mortality rate: 40.5%"
+    } else if (this.score == 6) {
+      this.classification1 = "DECAF score: 6 point."
+      this.classification2 = "Risk: high risk."
+      this.classification3 = "Recommendation: consider escalation of care vs. palliative care."
+      this.classification4 = "In-hospital mortality rate: 50%"
+    }
+  }
+  checkq1(event) {
+    this.q1 = event.target.value;
+    this.score = 0;
+    if (this.q1 == null || this.q2 == null || this.q3 == null || this.q4 == null || this.q5 == null){this.classification1='Please answer all of the questions!'}else{this.scoring();};
+  }
+  checkq2(event) {
+    this.q2 = event.target.value;
+    this.score = 0;
+    if (this.q1 == null || this.q2 == null || this.q3 == null || this.q4 == null || this.q5 == null){this.classification1='Please answer all of the questions!'}else{this.scoring();};
+  }
+  checkq3(event) {
+    this.q3 = event.target.value;
+    this.score = 0;
+    if (this.q1 == null || this.q2 == null || this.q3 == null || this.q4 == null || this.q5 == null){this.classification1='Please answer all of the questions!'}else{this.scoring();};
+  }
+  checkq4(event) {
+    this.q4 = event.target.value;
+    this.score = 0;
+    if (this.q1 == null || this.q2 == null || this.q3 == null || this.q4 == null || this.q5 == null){this.classification1='Please answer all of the questions!'}else{this.scoring();};
+  }
+  checkq5(event) {
+    this.q5 = event.target.value;
+    this.score = 0;
+    if (this.q1 == null || this.q2 == null || this.q3 == null || this.q4 == null || this.q5 == null){this.classification1='Please answer all of the questions!'}else{this.scoring();};
+  }
+
+  async presentAlert() {
+    const alert = await this.alertController.create({
+      header: 'Missing Value',
+      message: 'Please input all the required values!',
+      buttons: ['Okay']
+    });
+    await alert.present();
+  }
+
+  constructor(public alertController: AlertController) {
+  }
 
 }
